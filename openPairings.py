6d19e99da06b61b27127ab94e034e83db7264894
ubhqg <keanu.seiraffi@gmx.de>
2025-11-03 12:32:54 +0100

    changed hierachy

---
+++ openPairings.py
@@ -0,0 +1,27 @@
+from Helper import RunningDinnerHelper
+
+def get_teams():
+    helper = RunningDinnerHelper()
+    teams: list = []
+    for team in helper.teams:
+        teams.append(team[1])
+    return teams
+
+
+def open_pairings(teamName):
+    helper = RunningDinnerHelper()
+    pairings: list = []
+    for check_team in helper.teams:
+        if check_team[1] == teamName:
+            for team in get_teams():
+                if team not in check_team:
+                    pairings.append(team)
+            break
+    return pairings
+
+if __name__ == "__main__":
+    team = input("Enter team name: ")
+    pairings = open_pairings(team)
+    print(f"Possible pairings for team '{team}':")
+    for pairing in pairings:
+        print(f"- {pairing}")
